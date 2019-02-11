ac954b39e86c031824f39bdcf8b2d12527300314
hugo ple <hugo.ple2@gmail.com>
2019-02-11 18:31:25 +0100

    ex_scrabble

---
+++ app/controllers/games_controller_controller.rb
@@ -0,0 +1,8 @@
+class GamesControllerController < ApplicationController
+  def new
+    @letters = ('A'..'Z').to_a.sample(10)
+  end
+
+  def score
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'new', to: 'games_controller#new', as: :new
+  post 'score', to: 'games_controller#score', as: :score
 end
